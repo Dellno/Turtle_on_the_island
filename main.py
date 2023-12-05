ada28e2b91f4e8b12f96c9430e58a52b07166c84
dellno <89869798047t@gmail.com>
2023-12-05 22:07:39 +0500

    start programm

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import pygame
+
+
+def main():
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Turtle_on_the_island')
+    size = width, height = 800, 400
+    screen = pygame.display.set_mode(size)
+    main()
