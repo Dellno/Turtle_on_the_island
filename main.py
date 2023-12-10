30ebd1776a08e9e519c53af8a2e7cf36cf64be8c
dellno <89869798047t@gmail.com>
2023-12-10 14:27:00 +0500

    Class map render

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+from map import Map
 
 def main():
     running = True
@@ -14,6 +14,17 @@ def main():
 if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Turtle_on_the_island')
-    size = width, height = 800, 400
+    size = width, height = 1408, 640
     screen = pygame.display.set_mode(size)
-    main()
+    board = Map(256, 256, screen)
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                board.get_click(event.pos)
+        screen.fill((0, 0, 0))
+        board.render(20, 20, 0, 0)
+        pygame.display.flip()
+
