556f1eda8ebf47d647d3600bb765e06275f0911c
dellno <89869798047t@gmail.com>
2023-12-12 13:21:03 +0500

    water_block fix, map fix, main fix dirt_block

---
+++ island.py
@@ -0,0 +1,6 @@
+import pygame
+
+
+class GenerateIsland:
+    def __init__(self, x, y, width, height, set_map):
+        pass
--- main.py
+++ main.py
@@ -3,21 +3,14 @@ from map import Map
 
 
 def main():
-    running = True
-
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-        pygame.display.flip()
-
-
-if __name__ == '__main__':
-    pygame.init()
-    pygame.display.set_caption('Turtle_on_the_island')
     size = width, height = 1408, 640
     screen = pygame.display.set_mode(size)
     board = Map(256, 256, screen)
+
+    fps = 60
+    clock = pygame.time.Clock()
+
+
     running = True
     while running:
         for event in pygame.event.get():
@@ -26,5 +19,12 @@ if __name__ == '__main__':
             if event.type == pygame.MOUSEBUTTONDOWN:
                 board.get_click(event.pos)
         screen.fill((0, 0, 0))
-        board.render(20, 20, 0, 0)
+        board.render(128, 128, 0, 0)
         pygame.display.flip()
+        clock.tick(fps)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    pygame.display.set_caption('Turtle_on_the_island')
+    main()
