6aaa1dac42c688961cde668e2177dddb5e01df0b
Dellno <89869798047t@gmail.com>
2023-12-19 16:48:35 +0500

    turtle texture fix

--- dirt_block.py
+++ dirt_block.py
@@ -6,14 +6,13 @@ from block import Block
 class Grass(Block):
     def __init__(self, texture_type: int):
         self.texture_type = texture_type
-        match texture_type:
-            case 0:
-                super().__init__("ground_0.png")
-            case 1:
-                super().__init__("ground_1.png")
-            case 2:
-                super().__init__("ground_2.png")
-            case 3:
-                super().__init__("ground_3.png")
-            case 4:
-                super().__init__("ground_4.png")
+        if texture_type == 0:
+            super().__init__("ground_0.png")
+        if texture_type == 1:
+            super().__init__("ground_1.png")
+        if texture_type == 2:
+            super().__init__("ground_2.png")
+        if texture_type == 3:
+            super().__init__("ground_3.png")
+        if texture_type == 4:
+            super().__init__("ground_4.png")
