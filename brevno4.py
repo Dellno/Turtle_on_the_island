4db58fee3c2e3b1f8031258a5e84aa4f971a63e6
PC <roboyaz2006@gmail.com>
2023-12-26 12:55:45 +0500

    бревна нельзя брать

--- brevno4.py
+++ brevno4.py
@@ -4,4 +4,4 @@ from entity import Entity
 
 class Brevno_4(Entity):
     def __init__(self):
-        super().__init__("brevno_4", sprite="brevno4.png", hardness=0)
+        super().__init__("brevno_4", sprite="brevno4.png", hardness=-1)
