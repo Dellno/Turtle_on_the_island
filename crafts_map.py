bea223cada6ad55b7bd88cdca84073adef2b7e31
PC <roboyaz2006@gmail.com>
2023-12-26 10:40:13 +0500

    crafts

--- crafts_map.py
+++ crafts_map.py
@@ -1,4 +1,8 @@
 from sharp_stone import SharpStone
 CRAFTS_MAP = {
-    ("stone", "stone"): (None, "sharp_stone")
+    ("stone", "stone"): (None, "sharp_stone"),
+    ('sharp_stone', 'tree'): ('brevno', None),
+    (None, 'paporotnik'): ('thread', None),
+    (None, 'tree'): ('tree', 'stick'),
+    ('brevno', 'brevno', 'brevno', 'thread', 'thread'): ('plot')
 }  # сдесь записываются крафты в формате [entity_name, entity_name]: entity
