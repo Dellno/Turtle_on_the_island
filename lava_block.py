f5a99d4914e06a0991471f944122fb90b8184ae1
Аскар <sharafaskar@gmail.com>
2023-12-31 22:08:52 +0500

    circle island 2 by Askar

---
+++ lava_block.py
@@ -0,0 +1,12 @@
+from random import randint
+
+import pygame
+from block import Block
+
+
+class Magma(Block):
+    def __init__(self):
+        super().__init__("lava.png")
+
+    def block_event(self):
+        return "damage", -200
